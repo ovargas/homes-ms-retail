42ea4866246fcfd1bdc24cba6c06d9115933dc62
Orlando Vargas <6474589+ovargas@users.noreply.github.com>
2016-08-26 19:27:27 -0400

    Store API

---
+++ src/main/java/com/tenx/ms/retail/domain/StoreEntity.java
@@ -0,0 +1,43 @@
+package com.tenx.ms.retail.domain;
+
+
+import javax.validation.constraints.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "store")
+public class StoreEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "store_id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", length = 50)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public StoreEntity setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public StoreEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
---
+++ src/test/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,6 @@
+DELETE FROM store;
+
+INSERT INTO store (name) VALUES ('Store One');
+INSERT INTO store (name) VALUES ('Store Two');
+INSERT INTO store (name) VALUES ('Store To Delete');
+
