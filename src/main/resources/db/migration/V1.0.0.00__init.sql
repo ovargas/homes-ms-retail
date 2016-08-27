e528a212695b32fa1c25f7f975bc4fe4e214e683
Orlando Vargas <6474589+ovargas@users.noreply.github.com>
2016-08-26 22:38:37 -0400

    Product API

--- src/main/resources/db/migration/V1.0.0.00__init.sql
+++ src/main/resources/db/migration/V1.0.0.00__init.sql
@@ -1,4 +1,14 @@
 CREATE TABLE store (
     store_id BIGINT NOT NULL AUTO_INCREMENT COMMENT 'The id of the store',
-    name VARCHAR(50) COMMENT 'The name of the store'
-)
+    name VARCHAR(50) NOT NULL COMMENT 'The name of the store'
+);
+
+CREATE TABLE product (
+    product_id BIGINT NOT NULL AUTO_INCREMENT COMMENT 'The id of the product',
+    store_id BIGINT NOT NULL,
+    name VARCHAR(50) NOT NULL COMMENT 'The name of the product',
+    description TEXT NOT NULL COMMENT 'Description of the product',
+    sku VARCHAR(10) NOT NULL COMMENT 'Product SKU code',
+    price DECIMAL(20, 2) NOT NULL COMMENT 'Price of the product',
+    FOREIGN KEY (store_id) REFERENCES store (store_id)
+);
--- src/test/resources/db/migration/afterMigrate.sql
+++ src/test/resources/db/migration/afterMigrate.sql
@@ -1,6 +1,13 @@
+DELETE FROM product;
 DELETE FROM store;
 
+-- Stores
 INSERT INTO store (name) VALUES ('Store One');
 INSERT INTO store (name) VALUES ('Store Two');
 INSERT INTO store (name) VALUES ('Store To Delete');
 
+-- Products
+INSERT INTO product (store_id, name, description, sku, price) VALUES (1, 'Product One', 'Description of the product One', 'ABC11', 150.25);
+INSERT INTO product (store_id, name, description, sku, price) VALUES (1, 'Product Two', 'Description of the product Two', 'ABC12', 175.99);
+INSERT INTO product (store_id, name, description, sku, price) VALUES (1, 'Product Three', 'Description of the product Three', 'CFC12', 115.29);
+INSERT INTO product (store_id, name, description, sku, price) VALUES (1, 'Product to be deleted', 'Description of the product to be deleted', 'ABC13', 30.55);
