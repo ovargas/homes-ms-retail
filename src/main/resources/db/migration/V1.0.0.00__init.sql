2682093ad6048156b57f94588bdc9dcdf26bfbe6
Orlando Vargas <ovargas@mahisoft.com>
2016-08-29 15:36:48 -0400

    Stock API

--- src/main/resources/db/migration/V1.0.0.00__init.sql
+++ src/main/resources/db/migration/V1.0.0.00__init.sql
@@ -12,3 +12,11 @@ CREATE TABLE product (
     price DECIMAL(20, 2) NOT NULL COMMENT 'Price of the product',
     FOREIGN KEY (store_id) REFERENCES store (store_id)
 );
+
+CREATE TABLE stock (
+    --stock_id BIGINT AUTO_INCREMENT COMMENT  'Unique Id for the stock',
+    product_id BIGINT NOT NULL COMMENT 'The id of the product',
+    stock_count BIGINT NOT NULL COMMENT 'Total count in stock',
+    FOREIGN KEY (product_id) REFERENCES product (product_id),
+    PRIMARY KEY (product_id)
+);
